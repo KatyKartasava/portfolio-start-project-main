1fbd61a9be1304eaa75d97511c14f6d2186ae5b2
Katerina <katykartasova@gmail.com>
2024-03-05 17:32:39 +0100

    add face-fonts

---
+++ src/assets/layout/header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header>
+      <a href="#">Vahid Navazan</a>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contacts</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
